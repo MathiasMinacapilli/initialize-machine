1dd39991905e9b73aff475e95fd1fb386b11d0d2
Mathias Minacapilli <mathias.minacapilli@mercadolibre.com>
2022-04-15 04:22:47 -0700

    feat: first time initialize-machine script

---
+++ README.md
@@ -0,0 +1,4 @@
+# Initialize machine
+
+This script is intended to automate all new machine configurations
+and installations manually done.
---
+++ initialize-machine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script is intended to initialize a new machine installing all
+# utilities, programs, applications and configurations needed
+# for startup.
+
+'
+Programs:
+zsh
+oh-my-zsh (I think that this configures zsh as default shell)
+stow
+curl
+node
+'
